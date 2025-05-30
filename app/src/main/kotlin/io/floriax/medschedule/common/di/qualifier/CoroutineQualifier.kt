6df724bc35149e3ebc678fad93c9a951f6784b95
339 <339@339.im>
2025-05-30 19:04:13 +0800

    feat: Provide IO dispatcher using Hilt
    This commit introduces a Hilt module to provide the IO dispatcher for coroutines.
    
    - Added `IODispatcher` qualifier annotation.
    - Created `CoroutineModule` to provide `Dispatchers.IO` with the `IODispatcher` qualifier.
    
    Signed-off-by: 339 <339@339.im>

---
+++ app/src/main/kotlin/io/floriax/medschedule/common/di/qualifier/CoroutineQualifier.kt
@@ -0,0 +1,13 @@
+package io.floriax.medschedule.common.di.qualifier
+
+import javax.inject.Qualifier
+
+/**
+ *
+ *
+ * @author WangZhiYao
+ * @since 2025/5/11
+ */
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IODispatcher
