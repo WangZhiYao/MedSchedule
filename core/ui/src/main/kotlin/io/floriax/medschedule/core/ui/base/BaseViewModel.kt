2c7133775aa52d6a232b98366d52d17095ab3313
339 <339@339.im>
2025-06-26 22:49:35 +0800

    feat(ui): Add `BaseViewModel` and Composable extensions
    This commit introduces a `BaseViewModel` class and corresponding Composable extension functions to streamline UI state and side effect management.
    
    `BaseViewModel<STATE, SIDE_EFFECT>`:
    - Manages UI state (`StateFlow<STATE>`) and side effects (`SharedFlow<SIDE_EFFECT>`).
    - Provides `initialState` for subclasses to define the starting state.
    - Exposes `currentState` for synchronous access to the current state.
    - Offers `reduce(reducer: STATE.() -> STATE)` to update the state.
    - Provides `postSideEffect(sideEffect: SIDE_EFFECT)` to emit side effects.
    
    Composable extensions:
    - `collectState()`: Collects the `StateFlow` from the `BaseViewModel` as a Composable `State`, respecting the provided `Lifecycle.State`.
    - `collectSideEffect()`: Collects `SharedFlow` side effects from the `BaseViewModel` and executes a callback, respecting the provided `Lifecycle.State`.
    
    Signed-off-by: 339 <339@339.im>

---
+++ core/ui/src/main/kotlin/io/floriax/medschedule/core/ui/base/BaseViewModel.kt
@@ -0,0 +1,46 @@
+package io.floriax.medschedule.core.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+/**
+ *
+ *
+ * @author WangZhiYao
+ * @since 2025/5/30
+ */
+abstract class BaseViewModel<STATE : Any, SIDE_EFFECT : Any> : ViewModel() {
+
+    abstract val initialState: STATE
+
+    private val _state: MutableStateFlow<STATE> by lazy { MutableStateFlow(initialState) }
+    private val _sideEffect: MutableSharedFlow<SIDE_EFFECT> = MutableSharedFlow()
+
+    val state: StateFlow<STATE>
+        get() = _state.asStateFlow()
+
+    val sideEffect: SharedFlow<SIDE_EFFECT>
+        get() = _sideEffect.asSharedFlow()
+
+    protected val currentState: STATE
+        get() = state.value
+
+    protected fun reduce(reducer: STATE.() -> STATE) {
+        _state.update(reducer)
+    }
+
+    protected fun postSideEffect(sideEffect: SIDE_EFFECT) {
+        viewModelScope.launch {
+            _sideEffect.emit(sideEffect)
+        }
+    }
+
+}
