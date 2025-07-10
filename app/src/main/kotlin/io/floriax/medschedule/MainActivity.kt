4229b4fa983d3779e4428792edd907590529d623
339 <339@339.im>
2025-07-10 14:02:45 +0800

    debug(MainActivity): Add logging to lifecycle methods
    This commit adds logging to all standard Android Activity lifecycle methods in `MainActivity.kt`.
    
    This will help in debugging and understanding the activity's state transitions.
    
    The following methods now have debug logs:
    - `onCreate`
    - `onRestart`
    - `onStart`
    - `onResume`
    - `onSaveInstanceState`
    - `onRestoreInstanceState`
    - `onPause`
    - `onStop`
    - `onDestroy`
    
    Signed-off-by: 339 <339@339.im>

--- app/src/main/kotlin/io/floriax/medschedule/MainActivity.kt
+++ app/src/main/kotlin/io/floriax/medschedule/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import io.floriax.medschedule.core.common.extension.logger
 import io.floriax.medschedule.shared.designsystem.theme.AppTheme
 
 /**
@@ -17,7 +18,11 @@ import io.floriax.medschedule.shared.designsystem.theme.AppTheme
  */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val logger by logger<MainActivity>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        logger.d("onCreate")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -25,6 +30,46 @@ class MainActivity : ComponentActivity() {
                 MedScheduleApp()
             }
         }
+    }
+
+    override fun onRestart() {
+        logger.d("onRestart")
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        logger.d("onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        logger.d("onResume")
+        super.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        logger.d("onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        logger.d("onRestoreInstanceState")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onPause() {
+        logger.d("onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logger.d("onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logger.d("onDestroy")
+        super.onDestroy()
     }
 }
 
