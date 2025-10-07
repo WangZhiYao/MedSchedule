e6d01e6c81b2e5f0968c0b8af452af952114ff98
339 <339@339.im>
2025-10-07 20:19:06 +0800

    refactor(domain): Rename repository method for plan creation
    This commit renames the `createMedicationPlan` method to `create` in the `MedicationPlanRepository` interface.
    
    The `CreateMedicationPlanUseCase` has been updated to call the new `create` method, improving naming consistency within the domain layer. A KDoc author tag was also added to the use case.
    
    Signed-off-by: 339 <339@339.im>

--- core/domain/src/main/kotlin/io/floriax/medschedule/core/domain/usecase/CreateMedicationPlanUseCase.kt
+++ core/domain/src/main/kotlin/io/floriax/medschedule/core/domain/usecase/CreateMedicationPlanUseCase.kt
@@ -4,11 +4,17 @@ import io.floriax.medschedule.core.domain.model.MedicationPlan
 import io.floriax.medschedule.core.domain.repository.MedicationPlanRepository
 import javax.inject.Inject
 
+/**
+ *
+ *
+ * @author WangZhiYao
+ * @since 2025/10/6
+ */
 class CreateMedicationPlanUseCase @Inject constructor(
     private val medicationPlanRepository: MedicationPlanRepository
 ) : UseCase {
 
     suspend operator fun invoke(plan: MedicationPlan) =
-        medicationPlanRepository.createMedicationPlan(plan)
+        medicationPlanRepository.create(plan)
 
 }
